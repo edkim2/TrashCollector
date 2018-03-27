c4cbd0dbf15db384e3ef14a36ae0e738a6232824
edkim2 <ekim10203@gmail.com>
2018-03-27 12:06:51 -0500

    Created CustomersController

--- TrashCollector/Models/IdentityModels.cs
+++ TrashCollector/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace TrashCollector.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<TrashCollector.Models.Customer> Customers { get; set; }
     }
 }
