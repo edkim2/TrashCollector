adaf1c01d89b6b8b34c633b8bea86716c6a8925a
edkim2 <ekim10203@gmail.com>
2018-03-27 11:47:49 -0500

    Filled out Employee and Customer in 'Models'

--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -7,5 +7,18 @@ namespace TrashCollector.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StreeAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; }
     }
 }
